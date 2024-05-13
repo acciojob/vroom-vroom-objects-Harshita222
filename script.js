e083e429d6ddc3a693ed8b777cccdfcf3ba95553
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-13 19:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// Complete the js code
 class Car {
-constructor Car(make, model) {
-	
-		this.make = make;
-		this.model= model;
-	}
-	getMakeModel(){
-		  return this.make + ' ' + this.model;
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    getMakeModel() {
+        return this.make + ' ' + this.model;
     }
 }
 
-	class SportsCar extends Car{
-constructor (make, model, topSpeed) {
-	super(make,model);
-	constructor
-	this.topSpeed = topSpeed;
-}
-	getTopSpeed(){
-		return this.topSpeed;
-	}
-	
+class SportsCar extends Car {
+    constructor(make, model, topSpeed) {
+        super(make, model); // Call the parent class's constructor
+        this.topSpeed = topSpeed;
+    }
+
+    getTopSpeed() {
+        return this.topSpeed;
+    }
 }
 
 // Do not change the code below
